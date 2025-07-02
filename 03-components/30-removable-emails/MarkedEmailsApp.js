8044ff04922085be6104af12afd98cd68dece4aa
Sergey Kovin <skovin@techinfocom.com>
2025-07-02 08:33:37 +0500

    Задачи часть 3

--- 03-components/30-removable-emails/MarkedEmailsApp.js
+++ 03-components/30-removable-emails/MarkedEmailsApp.js
@@ -69,7 +69,7 @@ export default defineComponent({
       <UiFormGroup>
         <UiInput v-model.trim="query" type="search" placeholder="Поиск" aria-label="Поиск" small />
       </UiFormGroup>
-      <EmailList :emails="markedEmails" />
+      <EmailList :emails="markedEmails" @delete="removeEmailByIndex($event)">
     </div>
   `,
 })
