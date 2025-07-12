313c4a07df1864d7aab4366c708fac97394fb71b
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-07-12 09:50:27 +0300

    Added task UiFormGroup

---
+++ 05-ts/20-UiFormGroup/UiFormGroup.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="form-group">
+    <div class="form-group__label-wrapper">
+      <label for="FOR" class="form-group__label">LABEL</label>
+      <div class="form-group__description">DESCRIPTION</div>
+    </div>
+    <div class="form-group__control">CONTENT</div>
+    <div class="form-group__hint form-group__hint--invalid">HINT | ERROR</div>
+  </div>
+</template>
+
+<style scoped>
+/* _form-group.css */
+.form-group {
+}
+
+.form-group__label-wrapper {
+  margin-block-end: var(--spacing-small);
+}
+
+.form-group__label {
+  display: block;
+  font-size: var(--font-size-control);
+}
+
+.form-group__description {
+  color: var(--color-dimmed);
+}
+
+.form-group__hint {
+  font-size: var(--font-size-small);
+  color: var(--color-dimmed);
+  min-height: 1lh;
+
+  &.form-group__hint--invalid {
+    color: var(--color-danger);
+  }
+}
+</style>
